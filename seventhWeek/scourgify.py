0e72992eaa2f40c75c0927e6aeaa79fe64b14b00
Erik K <erik.kasperovic@mambu.com>
2025-03-13 18:29:11 +0200

    Pushing csv project

---
+++ seventhWeek/scourgify.py
@@ -0,0 +1,29 @@
+import csv
+import sys
+
+def checking_file():
+
+        if len(sys.argv) == 3:
+            if sys.argv[1].endswith(".csv"):
+                converting_names_and_houses(sys.argv[1], sys.argv[2])
+            else:
+                sys.exit("This is not a CSV file")
+        elif len(sys.argv) < 3:
+            sys.exit("Too few command-line arguments")
+        else:
+            sys.exit("Too many command-line arguments")
+
+
+def converting_names_and_houses(before_file, after_file):
+    with open(before_file) as file:
+        for line in file:
+            after = open(after_file, "a")
+            after.write("this is my first line")
+
+            after.close()
+
+
+def main():
+    checking_file()
+
+main()
